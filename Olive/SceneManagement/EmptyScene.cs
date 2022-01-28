fff4c5a21d6c16810a46bac50a57e0fc2481b4cc
Oliver Booth <me@olivr.me>
2022-01-28 13:44:56 +0000

    Throw exception when adding to empty scene

--- Olive/SceneManagement/EmptyScene.cs
+++ Olive/SceneManagement/EmptyScene.cs
@@ -5,4 +5,8 @@
 /// </summary>
 internal sealed class EmptyScene : Scene
 {
+    internal override void AddGameObject(GameObject gameObject)
+    {
+        throw new InvalidOperationException("Cannot add a game object to the empty scene.");
+    }
 }
