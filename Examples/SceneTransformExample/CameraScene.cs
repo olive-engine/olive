08ca3b60efb75fb5f58e3f4d1db5af22ee006484
Oliver Booth <me@olivr.me>
2022-01-29 12:57:07 +0000

    Add SceneTransformExample project

---
+++ Examples/SceneTransformExample/CameraScene.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Xna.Framework;
+using Olive;
+using Olive.SceneManagement;
+
+namespace SceneTransformExample;
+
+internal sealed class CameraScene : Scene
+{
+    protected override void Initialize()
+    {
+        base.Initialize();
+
+        MainCamera.Transform.Position = Vector3.Backward * 2;
+        Camera.Main = MainCamera;
+    }
+}
---
+++ Examples/SceneTransformExample/MonkeyPlaneScene.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Olive;
+using Olive.Rendering;
+using Olive.SceneManagement;
+
+namespace SceneTransformExample;
+
+internal sealed class MonkeyPlaneScene : Scene
+{
+    private float _time;
+
+    protected override void Initialize()
+    {
+        base.Initialize();
+
+        // we don't want more than one camera. let the camera in CameraScene do the work
+        MainCamera.GameObject.Dispose();
+    }
+
+    protected override void LoadContent()
+    {
+        base.LoadContent();
+
+        var plane = new GameObject(this);
+        plane.Transform.Position = Vector3.Down * 3;
+        plane.AddComponent<ModelRenderer>().Model = LoadContent<Model>("Plane");
+
+        var centerMonkey = new GameObject(this);
+        centerMonkey.Transform.Scale = Vector3.One * 0.0005f;
+        centerMonkey.AddComponent<ModelRenderer>().Model = LoadContent<Model>("monkey");
+    }
+
+    protected override void Update(GameTime gameTime)
+    {
+        base.Update(gameTime);
+
+        _time = (float) (_time + gameTime.ElapsedGameTime.TotalSeconds) % (MathF.PI * 2);
+        Transform.Position = Vector3.Up * MathF.Sin(_time);
+    }
+}
